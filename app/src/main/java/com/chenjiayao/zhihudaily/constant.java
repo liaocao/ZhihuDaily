f9749eb63999362815893ba92a2a585a9f688a10
chenjiayao <mail_one_piece@163.com>
2015-11-29 21:15:33 +0800

    add

--- app/src/main/java/com/chenjiayao/zhihudaily/constant.java
+++ app/src/main/java/com/chenjiayao/zhihudaily/constant.java
@@ -3,7 +3,7 @@ package com.chenjiayao.zhihudaily;
 /**
  * Created by chen on 2015/11/15.
  */
-public class constant {
+public class Constant {
 
     public static final String START_URL = "http://news-at.zhihu.com/api/4/";
     public static final String BEFORE_URL = "news/before/";
